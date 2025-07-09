92b699d3b0dbbaf2f70e8e6e0510e1610d0bd61e
Naveen Joy <naveenjoy300@gmail.com>
2025-07-09 11:06:29 +0200

    s

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from './HeroStyles.module.css'
-import heroImg from '../../assets/pp.png'
+import heroImg from '../../assets/neww.png'
 import sun from '../../assets/sun.svg'
 import moon from '../../assets/moon.svg'
 import twitterLight from '../../assets/twitter-light.svg'
